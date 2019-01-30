d642aa85335a6e8f575ee819038fb954b72d9122
Carlos Frigo <carlos.frigo@gmail.com>
2019-01-30 17:52:17 -0200

    Nessa aula, aprendemos:
    Como funciona a pilha de chamadas (CallStack) no .NET;
    Propagar erros com retorno de métodos não é correto;
    Podemos usar as exceções do .NET;
    Para tratar exceções do .NET, basta usar os blocos try/catch;
    As exceções são representadas por objetos e duas propriedades importantes são a Message e StackTrace.
    Mas, este é só o começo. Na próxima aula vamos aprender mais sobre
    tratamento de exceções e a pilha de chamadas. Vamos lá?

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -10,7 +10,39 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Metodo();
+            }
+            catch (DivideByZeroException excecao)
+            {
+                Console.WriteLine(excecao.Message);
+                Console.WriteLine(excecao.StackTrace);
+            }
+            catch (NullReferenceException excecao)
+            {
+                Console.WriteLine(excecao.Message);
+                Console.WriteLine(excecao.StackTrace);
+            }
+
             Console.ReadLine();
+        }
+
+        public static int Dividir(int numero, int divisor)
+        {
+            ContaCorrente conta = null;
+            Console.WriteLine(conta.Saldo);
+            return numero / divisor;
+        }
+
+        static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        static void TestaDivisao(int divisor)
+        {
+            Dividir(10, divisor);
         }
     }
 }
